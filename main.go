fe525fc79278ca2f8c0beeeb304344f2952bc0c3
esh4d0w <jan.reinhard@pm.me>
2025-07-27 15:00:44 +0200

    Newlines

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -19,16 +20,32 @@ func main() {
 
 	log.Printf("Reading Data from %s:\n ---", inputFilePath)
 
+	currentLine := ""
 	for {
 		buffer := make([]byte, 8)
 		n, err := file.Read(buffer)
 		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
 			if errors.Is(err, io.EOF) {
 				break
 			}
 			log.Printf("error read: %s\n", err)
 			break
 		}
-		fmt.Printf("read: %s\n", string(buffer[:n]))
+		currentStr := string(buffer[:n])
+		parts := strings.Split(currentStr, "\n")
+		// if len(parts) > 1 {
+		// 	fmt.Printf("read:%s%s\n", currentLine, parts[0])
+		// 	currentLine = ""
+		// }
+		// Commented is my solution the one below is the from boot.dev
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLine, parts[i])
+			currentLine = ""
+		}
+		currentLine += parts[len(parts)-1]
 	}
 }
