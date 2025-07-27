dfe06566d623ad5fdc3875ff1500e231f69bad24
esh4d0w <jan.reinhard@pm.me>
2025-07-27 15:03:45 +0200

    Channel Refactor

--- main.go
+++ main.go
@@ -16,36 +16,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't open %s: %s\n", inputFilePath, err)
 	}
-	defer file.Close()
 
 	log.Printf("Reading Data from %s:\n ---", inputFilePath)
 
-	currentLine := ""
-	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
+	linesChannel := getLinesChannel(file)
+
+	for line := range linesChannel {
+		log.Printf("read: %s\n", line)
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					lines <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Printf("error read: %s\n", err)
 				break
 			}
-			log.Printf("error read: %s\n", err)
-			break
+			currentStr := string(buffer[:n])
+			parts := strings.Split(currentStr, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLine, parts[i])
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
 		}
-		currentStr := string(buffer[:n])
-		parts := strings.Split(currentStr, "\n")
-		// if len(parts) > 1 {
-		// 	fmt.Printf("read:%s%s\n", currentLine, parts[0])
-		// 	currentLine = ""
-		// }
-		// Commented is my solution the one below is the from boot.dev
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLine, parts[i])
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
-	}
+	}()
+	return lines
 }
