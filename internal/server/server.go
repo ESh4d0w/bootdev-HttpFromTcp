bfc3a4458091d57d30e4f09d06f06099b6fa858d
esh4d0w <jan.reinhard@pm.me>
2025-08-04 13:32:45 +0200

    Handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,26 +1,46 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync/atomic"
 
+	"github.com/esh4d0w/bootdev-HttpFromTcp/internal/request"
 	"github.com/esh4d0w/bootdev-HttpFromTcp/internal/response"
 )
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (he HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, he.StatusCode)
+	message := []byte(he.Message)
+	defaultHeaders := response.GetDefaultHeaders(len(message))
+	_ = response.WriteHeaders(w, defaultHeaders)
+	w.Write(message)
+}
+
 type Server struct {
 	listener net.Listener
+	handler  Handler
 	closed   atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
 	}
 	server := &Server{
 		listener: listener,
+		handler:  handler,
 	}
 	go server.listen()
 	return server, nil
@@ -50,10 +70,25 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	response.WriteStatusLine(conn, response.StatusOK)
-	defaultHeaders := response.GetDefaultHeaders(0)
-	err := response.WriteHeaders(conn, defaultHeaders)
+	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		log.Printf("Error Responindg: %v\n", err)
+		hErr := &HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
+		return
 	}
+	buffer := bytes.NewBuffer([]byte{})
+	hErr := s.handler(buffer, req)
+	if hErr != nil {
+		hErr.Write(conn)
+		return
+	}
+
+	body := buffer.Bytes()
+	response.WriteStatusLine(conn, response.StatusOK)
+	headers := response.GetDefaultHeaders(len(body))
+	_ = response.WriteHeaders(conn, headers)
+	conn.Write(body)
 }
